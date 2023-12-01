0aaea966a02f0ca4867a545b7074e9b4dad46973
Lucas Victor <128725402+lucasvictorm@users.noreply.github.com>
2023-11-30 21:55:28 -0300

    Preparação para receber db

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,5 +1,8 @@
-const getAll = () => {
+const connection = require('./connection.js')
 
+const getAll = async () => {
+    const tasks = await connection.execute("SELECT * FROM tasks")
+    return tasks;
 };
 
 module.exports = {
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,9 @@
 const express = require('express')
+const tasksController = require('./controllers/tasksController')
 
 const router = express.Router()
 
-/*router.get('/tasks', (req, res) => {
-    res.status(200).send("Router funcionando")
-})*/
+router.get('/tasks', tasksController.getAll)
 
 
 module.exports = router
